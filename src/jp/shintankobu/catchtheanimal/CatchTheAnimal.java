3f1062f801a074dca95ff3278e30967ad21a8b3c
shintankobu <shintankobu@gmail.com>
2014-09-09 02:53:59 +0900

    hello worldを表示します。

---
+++ src/jp/shintankobu/catchtheanimal/CatchTheAnimal.java
@@ -0,0 +1,15 @@
+package jp.shintankobu.catchtheanimal;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CatchTheAnimal extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.game);
+	}
+
+
+}
